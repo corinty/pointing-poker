87cc093194140902ca3993f5ff99eb4d110389eb
Corin McHargue <corinty@users.noreply.github.com>
2024-02-02 08:15:53 -0600

    Added prettier formatting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "plugin:@shopify/esnext",
+    "plugin:@shopify/react",
+    "plugin:@shopify/typescript",
+    "plugin:@shopify/react",
+    "plugin:@shopify/prettier",
+  ],
+  parserOptions: {
+    project: "tsconfig.json",
+  },
 };
--- app/routes/room.$roomId.tsx
+++ app/routes/room.$roomId.tsx
@@ -1,47 +1,55 @@
-import { LinksFunction } from "@remix-run/node";
-import { useParams } from "@remix-run/react";
-import { doc, setDoc } from "firebase/firestore";
-import { Fragment, useEffect, useState } from "react";
-import Confetti from "react-confetti";
-import CopyCurrentUrlToClipboard from "~/components/CopyCurrentUrlToClipboard";
-import { db } from "~/db/firestore";
-import { useActiveStory } from "~/hooks/useActiveStory";
-import { useCurrentUser } from "~/hooks/useCurrentUser";
-import { usePresence } from "~/hooks/usePresence";
-import { usePresentUsers } from "~/hooks/usePresentUsers";
-import { useRoom } from "~/hooks/useRoom";
-import styles from "~/styles/room.css";
-import { useWindowSize } from "~/utils/useWindowSize";
-import _ from "lodash";
-import { storyRepository } from "~/db/stories";
+import {LinksFunction} from '@remix-run/node';
+import {useParams} from '@remix-run/react';
+import {doc, setDoc} from 'firebase/firestore';
+import {Fragment, useEffect, useState} from 'react';
+import Confetti from 'react-confetti';
+import CopyCurrentUrlToClipboard from '~/components/CopyCurrentUrlToClipboard';
+import {db} from '~/db/firestore';
+import {useActiveStory} from '~/hooks/useActiveStory';
+import {useCurrentUser} from '~/hooks/useCurrentUser';
+import {usePresence} from '~/hooks/usePresence';
+import {usePresentUsers} from '~/hooks/usePresentUsers';
+import {useRoom} from '~/hooks/useRoom';
+import styles from '~/styles/room.css';
+import {useWindowSize} from '~/utils/useWindowSize';
+import _ from 'lodash';
+import {storyRepository} from '~/db/stories';
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}];
 
 const pointValues = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100];
 
 export default function Room() {
-  const { roomId } = useParams();
-  const { width, height } = useWindowSize();
+  const {roomId} = useParams();
+  const {width, height} = useWindowSize();
   const [shouldShowConfetti, setConfetti] = useState(false);
   const currentUser = useCurrentUser();
 
   // TODO: Redirect to login page
-  if (!currentUser) throw Error("Must be logged In");
-  if (!roomId) throw Error("missing param");
+  if (!currentUser) throw Error('Must be logged In');
+  if (!roomId) throw Error('missing param');
 
-  const { room, stories, loading } = useRoom(roomId);
+  const {room, stories, loading} = useRoom(roomId);
 
-  const { data: usersData, loading: usersLoading } = usePresentUsers(roomId);
-  const { data: activeStory, submitVote, currentUserVote, clearVotes, nextStory } = useActiveStory(roomId, room?.activeStoryId);
+  const {data: usersData, loading: usersLoading} = usePresentUsers(roomId);
+  const {
+    data: activeStory,
+    submitVote,
+    currentUserVote,
+    clearVotes,
+    nextStory,
+  } = useActiveStory(roomId, room?.activeStoryId);
 
   useEffect(() => {
     if (!activeStory) return;
     const votes = Object.values(activeStory.votes);
-    const hasConsensus = usersData?.length == votes.length && votes.every((vote) => vote == votes[0]);
+    const hasConsensus =
+      usersData?.length == votes.length &&
+      votes.every((vote) => vote == votes[0]);
 
     setConfetti(hasConsensus);
   }, [activeStory]);
-  console.log({ room, activeStory, usersLoading });
+  console.log({room, activeStory, usersLoading});
   if (loading || !room || !activeStory || usersLoading) return;
 
   return (
@@ -53,7 +61,7 @@ export default function Room() {
       <div className="flex gap-2 mb-2">
         <textarea
           className="m-0 min-h-32 w-1/2"
-          value={activeStory?.description || ""}
+          value={activeStory?.description || ''}
           onChange={async (e) => {
             storyRepository.updateStory(roomId, room.activeStoryId, {
               description: e.target.value,
@@ -61,7 +69,7 @@ export default function Room() {
           }}
         />
         <button
-          style={{ height: "100%", margin: "0" }}
+          style={{height: '100%', margin: '0'}}
           onClick={() => {
             nextStory();
           }}
@@ -78,12 +86,16 @@ export default function Room() {
           }}
         >
           Clear Votes
-        </button>{" "}
+        </button>{' '}
         <button className="flex-grow bg-green-600">Show Votes</button>
       </div>
       <div className="points">
         {pointValues.map((value) => (
-          <button key={value} className={value == currentUserVote ? "bg-green-500" : ""} onClick={() => submitVote(currentUser.uid, value)}>
+          <button
+            key={value}
+            className={value == currentUserVote ? 'bg-green-500' : ''}
+            onClick={() => submitVote(currentUser.uid, value)}
+          >
             {value}
           </button>
         ))}
@@ -101,15 +113,19 @@ export default function Room() {
                   <img src={player.photoURL} />
                 </div>
               </div>
-              {activeStory.displayVotes ? <div className="text-9xl">{activeStory?.votes[player.uid]}</div> : <div className="text-9xl bg-slate-900 h-2/3 w-2/3"></div>}
+              {activeStory.displayVotes ? (
+                <div className="text-9xl">{activeStory?.votes[player.uid]}</div>
+              ) : (
+                <div className="text-9xl bg-slate-900 h-2/3 w-2/3"></div>
+              )}
             </Fragment>
           );
         })}
       </div>
 
       {shouldShowConfetti && (
-        <div style={{ display: "fixed", top: 0, left: 0 }}>
-          {" "}
+        <div style={{display: 'fixed', top: 0, left: 0}}>
+          {' '}
           <Confetti width={width} height={height} />
         </div>
       )}
