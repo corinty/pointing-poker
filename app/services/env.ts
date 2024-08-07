d53689b038552ed43b7712bd0036b091e4695a81
Corin McHargue <corin.mchargue@shopify.com>
2024-08-07 16:04:19 -0500

    Remove unneeded env validation

--- app/services/env.ts
+++ app/services/env.ts
@@ -4,9 +4,6 @@ import {z} from 'zod';
 export const zodEnv = z.object({
   // Database
   DATABASE_URL: z.string(),
-  // Cloudflare
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
   // Secret
   SESSION_SECRET: z.string(),
 });
